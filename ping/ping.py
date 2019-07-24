ce6c1eef1d765d8177d6bab5331d48bf04601d6e
Eric De Witte <edewitte@vmware.com>
2019-07-24 09:41:08 +0200

    adding hostname

--- ping/ping.py
+++ ping/ping.py
@@ -1,10 +1,14 @@
 import os
+import socket
 from flask import Flask
 app = Flask(__name__)
 
+podname = socket.gethostname()
+
 @app.route("/")
 def hello():
-    return "ping!"
+    message = "pong - " + podname 
+    return message
 
 if __name__ == "__main__":
 	app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
--- pong/pong.py
+++ pong/pong.py
@@ -1,12 +1,14 @@
-import os 
-
+import os
+import socket
 from flask import Flask
 app = Flask(__name__)
 
+podname = socket.gethostname()
+
 @app.route("/")
 def hello():
-    return "pong!"
+    message = "pong - " + podname 
+    return message
 
 if __name__ == "__main__":
 	app.run(debug=False,host='0.0.0.0', port=int(os.getenv('PORT', '5000')))
-
